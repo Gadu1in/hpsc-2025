7b0cbfbf3113e0b85eb3917610181a5e71803441
Oskar Höglund <48620643+Gadu1in@users.noreply.github.com>
2025-04-15 20:20:24 +0900

    Homework 1, Student ID: 24R50510

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -1,12 +1,14 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <chrono>
+#include <omp.h>
 
 void merge(std::vector<int>& vec, int begin, int mid, int end) {
   std::vector<int> tmp(end-begin+1);
   int left = begin;
   int right = mid+1;
-  for (int i=0; i<tmp.size(); i++) { 
+  for (int i=0; i<tmp.size(); i++) {
     if (left > mid)
       tmp[i] = vec[right++];
     else if (right > end)
@@ -14,20 +16,35 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
     else if (vec[left] <= vec[right])
       tmp[i] = vec[left++];
     else
-      tmp[i] = vec[right++]; 
+      tmp[i] = vec[right++];
   }
-  for (int i=0; i<tmp.size(); i++) 
+  for (int i=0; i<tmp.size(); i++)
     vec[begin++] = tmp[i];
 }
 
 void merge_sort(std::vector<int>& vec, int begin, int end) {
   if(begin < end) {
     int mid = (begin + end) / 2;
+
+    if(end - begin > 2000) { // Cutoff to not be limited by overhead, at some point its better to run
+                             // the code in serial instead.
+#pragma omp task shared(vec, begin)
+    printf("Thread %d computing merge_sort(vec=vec, begin=%d, mid=%d)\n", omp_get_thread_num(), begin, mid);
     merge_sort(vec, begin, mid);
+#pragma omp task shared(vec, end)
+    printf("Thread %d computing merge_sort(vec=vec, mid=%d, end=%d)\n", omp_get_thread_num(), mid+1, end);
     merge_sort(vec, mid+1, end);
+#pragma omp taskwait
     merge(vec, begin, mid, end);
+    }
+    else {
+        merge_sort(vec, begin, mid);
+        merge_sort(vec, mid+1, end);
+        merge(vec, begin, mid, end);
+    }
   }
 }
+
 
 int main() {
   int n = 20;
@@ -37,9 +54,14 @@ int main() {
     printf("%d ",vec[i]);
   }
   printf("\n");
+  auto tic = std::chrono::steady_clock::now();
+#pragma omp parallel
+#pragma omp single
   merge_sort(vec, 0, n-1);
+  auto toc = std::chrono::steady_clock::now();
+  double time = std::chrono::duration<double>(toc - tic).count();
   for (int i=0; i<n; i++) {
     printf("%d ",vec[i]);
   }
-  printf("\n");
+  printf("\n t=%f \n", time);
 }
--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <chrono>
+#include <omp.h>
 
 int main() {
   int n = 50;
@@ -12,18 +14,31 @@ int main() {
   }
   printf("\n");
 
-  std::vector<int> bucket(range,0); 
+  std::vector<int> bucket(range,0);
+
+  auto tic = std::chrono::steady_clock::now();
+
   for (int i=0; i<n; i++)
-    bucket[key[i]]++;
+    bucket[key[i]]++; // Accumulates how many entries we have in each bucket
   std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+  for (int i=1; i<range; i++)
+    offset[i] = offset[i-1] + bucket[i-1]; // The total number of elements to get from the first
+                                           // element to the entry after the last element in bucket i
+                                           // bucket    = [2] [1] [4] [0] [2] [3]  [2]  [4]
+                                           // -> offset = [0] [2] [3] [7] [7] [9] [12] [14]
   for (int i=0; i<range; i++) {
     int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
+#pragma omp parallel for
+    for (int bucket_n=0; bucket_n<bucket[i]; bucket_n++) {
+      key[j + bucket_n] = i;
+      printf("Thread %d computing key[j=%d, bucket=%d] = i=%d \n", omp_get_thread_num(), j, bucket_n, i);
     }
   }
+
+  auto toc = std::chrono::steady_clock::now();
+  double time = std::chrono::duration<double>(toc - tic).count();
+
+  printf("n=%d, t=%f\n", n, time);
 
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
